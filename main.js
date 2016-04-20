b05756ef04331a1efdb61a0629b0aa05befe24d8
Dmitry <Dmitry@MacBook-Pro.local>
2016-04-20 19:19:51 +0300

    clickable version first commit

--- main.js
+++ main.js
@@ -1,27 +1,19 @@
 var CELL_SIDE = 38;
-var BORDER_THICKNESS = 1;
-var CELL_STEP = CELL_SIDE + BORDER_THICKNESS * 2;
+var BORDER_THICK = 1;
+var CELL_STEP = CELL_SIDE + BORDER_THICK * 2;
 var ACTIVE_CELL_COLOR = "#C2FFA3";
 var INACTIVE_CELL_COLOR = "#7A6935";
-var PLAYER_COLOR = "#F3D921";
-var ENEMY_COLOR = "#2196F3";
-var PLAYER_SPEED = 0.3;
-var ENEMY_SPEED = 0.3;
-var PLAYER_INTERVAL_VAL = 400;
-var ENEMY_INTERVAL_VAL = 500;
+var MARKED_CELL_COLOR = "rgb(126, 228, 74)";
+var RED_CELL_COLOR = "#FF2F00";
 var activeCells = [];
-var enemy;
-var player;
+var person;
 var currentTarget;
-var firstMove = true;
-var target;
-var keyCode;
-var interval1;
-var interval2;
+var isComplete = true;
+var tl = new TimelineLite({onComplete:function(){ isComplete = true }});
 
 var map = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
   [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
   [1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1],
@@ -40,115 +32,32 @@ var map = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
-function Person(params){
-  return this.init(params);
-}
-
-Person.prototype.init = function(params){
-  this.div = document.createElement('div');
-  this.x = params.x;
-  this.y = params.y;
-  this.div.style.position = "absolute";
-  this.div.style.left = params.x + 'px';
-  this.div.style.top = params.y + 'px';
-  this.div.style.width = CELL_STEP + "px";
-  this.div.style.height = CELL_STEP + "px";
-  this.div.style.borderRadius = "50%";
-  this.div.style.backgroundColor = params.color;
-  document.body.appendChild(this.div);
-}
-
 function init() {
 
   drawTheMap();
 
-  player = new Person({
-    x: activeCells[activeCells.length-1].x,
-    y: activeCells[activeCells.length-1].y,
-    color: PLAYER_COLOR
-  });
+  person = document.createElement('div');
 
-  enemy = new Person({
-    x: activeCells[0].x,
-    y: activeCells[0].y,
-    color: ENEMY_COLOR
-  });
+  person.x = activeCells[0].x;
+  person.y = activeCells[0].y;
 
-  window.addEventListener("keydown", keyDown);
-  window.addEventListener("keyup", keyUp);
+  person.style.position = "absolute";
+  person.style.left = activeCells[0].x + 'px';
+  person.style.top = activeCells[0].y + 'px';
+  person.style.width = CELL_STEP + "px";
+  person.style.height = CELL_STEP + "px";
+  person.style.borderRadius = "50%";
+  person.style.backgroundColor = "#2196F3";
 
-  interval1 = setInterval(calculateThePathTo, ENEMY_INTERVAL_VAL);
-  interval2 = setInterval(checkTheKeyEvents, PLAYER_INTERVAL_VAL);
-}
-
-function gameOver(){
-  clearInterval(interval1);
-  clearInterval(interval2);
-  window.removeEventListener("keydown", keyDown);
-  window.removeEventListener("keyup", keyUp);
-  alert("GAME OVER :(");
-}
-
-function keyDown(e) {
-  keyCode = e.keyIdentifier;
-}
-
-function keyUp(e) {
-  keyCode = undefined;
-}
-
-function checkTheKeyEvents(){
-
-  if(!keyCode) return;
-
-  var currentX = player.x;
-  var currentY = player.y;
-  var point;
-
-  if(keyCode === "Left") {
-    animateThePlayer({ x:currentX - CELL_STEP, y:currentY });
-    return;
-  }
-  else if(keyCode === "Right") {
-    animateThePlayer({ x:currentX + CELL_STEP, y:currentY });
-    return;
-  }
-  else if(keyCode === "Up") {
-    animateThePlayer({ x:currentX, y:currentY - CELL_STEP });
-    return;
-  }
-  else if(keyCode === "Down") {
-    animateThePlayer({ x:currentX, y:currentY + CELL_STEP });
-    return;
-  }
-}
-
-function animateThePlayer(points) {
-
-  function isActiveCell(element, index, array) {
-    return element.x === points.x && element.y === points.y
-  }
-
-  var nextStep = activeCells.find(isActiveCell);
-
-  if(!nextStep) return;
-
-  target = nextStep;
-  firstMove = false;
-
-  TweenMax.to(player.div, PLAYER_SPEED, {left:points.x, top:points.y, ease:Power0.easeNone, overwrite:0, onComplete:function(){
-    player.x = points.x;
-    player.y = points.y;
-  }});
-
+  document.body.appendChild(person);
 }
 
 function drawTheMap() {
   for (var a = 0; a < map.length; a++) {
     for (var b = 0; b < map[a].length; b++) {
       var position = {
-        x: CELL_STEP * b,
-        y: CELL_STEP * a
+        x: CELL_STEP * a,
+        y: CELL_STEP * b
       };
       map[a][b] === 1 ? new Rectangle(position) : new ActiveRectangle(position);
     }
@@ -156,6 +65,7 @@ function drawTheMap() {
 }
 
 function Rectangle(params) {
+  // console.log('static rect');
   this.color = INACTIVE_CELL_COLOR;
   this.init(params);
 }
@@ -168,24 +78,32 @@ Rectangle.prototype.init = function(params) {
   this.div.style.height = CELL_SIDE + "px";
   this.div.style.backgroundColor = this.color;
   this.div.style.position = "absolute";
-  this.div.style.border = BORDER_THICKNESS+"px solid #ffffff";
-  this.div.style.top = params.y + "px";
-  this.div.style.left = params.x + "px";
+  this.div.style.border = BORDER_THICK+"px solid #ffffff";
+  this.div.style.top = params.x + "px";
+  this.div.style.left = params.y + "px";
   document.body.appendChild(this.div);
 }
 
 function ActiveRectangle(params) {
+  // console.log('active rect');
   var self = this;
   this.color = ACTIVE_CELL_COLOR;
   this.init(params);
+  this.div.addEventListener("click", function(){
+    calculateThePathTo(self);
+  });
   activeCells.push(this);
 }
 
 ActiveRectangle.prototype = Object.create(Rectangle.prototype);
 
-function calculateThePathTo() {
+function calculateThePathTo(target) {
 
-  if(firstMove) return;
+  if (target !== currentTarget) {
+    if(currentTarget) currentTarget.div.style.backgroundColor = ACTIVE_CELL_COLOR;
+    target.div.style.backgroundColor = RED_CELL_COLOR;
+    currentTarget = target;
+  }
 
   var cellsList = activeCells.slice();
   var counter = 0;
@@ -194,22 +112,35 @@ function calculateThePathTo() {
   var path = [];
 
   loop();
-  buildThePath();
-  animateTheEnemy();
+  buildAPath();
+  //colorThePath();
+  animateThePerson();
 
-  function animateTheEnemy(){
+  // TweenLite.ticker.addEventListener("tick", function(){
+  //   console.log(person.x+"/"+person.y);
+  // });
+
+  function animateThePerson(){
+    tl.clear();
     var list = path.reverse();
-    var item = list[0];
-    if(item.x === player.x && item.y === player.y) {
-      gameOver();
+    for(var i = 0; i < list.length; i++) tl.add(returnNewMove(list[i]));
+    function returnNewMove(item){
+      return TweenMax.to(person, 0.15, {left:item.y, top:item.x, ease:Power0.easeNone, overwrite:0, onComplete:function(){
+        person.x = item.x;
+        person.y = item.y;
+      }});
     }
-    TweenMax.to(enemy.div, ENEMY_SPEED, {left:item.x, top:item.y, ease:Power0.easeNone, overwrite:0, onComplete:function(){
-      enemy.x = item.x;
-      enemy.y = item.y;
-    }});
   }
 
-  function buildThePath() {
+  function colorThePath() {
+    var list = []
+    for (var i = 0; i < path.length; i++) list.push(path[i].div);
+    list = list.reverse();
+    list.splice(list.length-1, 1);
+    TweenMax.staggerTo(list, 1, {backgroundColor:MARKED_CELL_COLOR, repeat:1, yoyo:true, ease:Power3.easeOut}, 0.05);
+  }
+
+  function buildAPath() {
     var currentCell;
     var testList = sortCells(cells);
     var counter = cellsList.length;
@@ -228,7 +159,7 @@ function calculateThePathTo() {
         }
       }
 
-      if((currentCell.x === enemy.x && currentCell.y === enemy.y) || counter < 1) return;
+      if((currentCell.x === person.x && currentCell.y === person.y) || counter < 1) return;
 
       counter--;
       loop();
@@ -246,11 +177,14 @@ function calculateThePathTo() {
     if(cellsList.length === 0) return;
 
     for (var i = 0; i < cellsList.length; i++) {
-      var step = cellsList[i];
-      steps[counter] = steps[counter] || [];
 
+      var step = cellsList[i];
+
+      steps[counter] =  steps[counter] || [];
+
+      // first run
       if (steps.length <= 1 ) {
-        if (match(step, enemy)) pushElem(step);
+        if (match(step, person)) pushElem(step);
       }
       else {
         for (var a = 0; a < steps[counter-1].length; a++) {
@@ -272,6 +206,7 @@ function calculateThePathTo() {
     steps[counter].push(step);
     cells.push(step);
     step.id = counter;
+    if(step.x === target.x && step.y === target.y) return;
   }
 
   function match(step, target) {
